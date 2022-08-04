9514b0d4f44d499baacaba92119e1605ed0b6447
songjuyoung-1410 <chicken010208@gmail.com>
2022-08-04 11:17:20 +0900

    디자인 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Click from './component/Click.js';
-import Header from './component/Header.js';
-import './App.css';
-
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Click from "./component/Click.js";
+import Header from "./component/Header.js";
+import "./App.css";
 
 function App() {
-    return (
-      <div className='App'>
-        <Header />
-        <Click/>
-      </div>
-    );
-  };
+  return (
+    <div className="App">
+      <Header />
+      <Click />
+    </div>
+  );
+}
 
 export default App;
